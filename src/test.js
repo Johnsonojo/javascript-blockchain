beb29ef0191db1bfe923536393cf83ce0856beb3
Johnson Ojo <johnsonojo89@gmail.com>
2020-01-08 09:41:35 +0100

    add blockchain network synchronization

--- src/test.js
+++ src/test.js
@@ -1,31 +1,29 @@
-const Blockchain = require("./blockchain/blockchain");
+const Blockchain = require('./blockchain/blockchain');
 const bitcoin = new Blockchain();
 
 const previousBlockHash = 'hgcjkccdcdbgdkklj';
 const currentBlockDetails = [
   {
-      amount: 300,
-      sender: 'cvdcjvjhcvjcgegwediu',
-      recipient: 'fwufgisvisisivvcvvc'
-    },
-    {
-      amount: 500,
-      sender: 'vcvcdvcvcjvccvcjhvchj',
-      recipient: 'hcvjhcjvcjhcjhckhckknchi'
-    },
-    {
-      amount: 700,
-      sender: 'gfgwfyuwewdiuigid',
-      recipient: 'gefyefihdgdqdBHMJ'
-    },
-    {
-      amount: 900,
-      sender: 'YERROpsbjdfvcvwebfy',
-      recipient: 'ieufyygivninbsxqoPjvg'
-    }
+    amount: 300,
+    sender: 'cvdcjvjhcvjcgegwediu',
+    recipient: 'fwufgisvisisivvcvvc'
+  },
+  {
+    amount: 500,
+    sender: 'vcvcdvcvcjvccvcjhvchj',
+    recipient: 'hcvjhcjvcjhcjhckhckknchi'
+  },
+  {
+    amount: 700,
+    sender: 'gfgwfyuwewdiuigid',
+    recipient: 'gefyefihdgdqdBHMJ'
+  },
+  {
+    amount: 900,
+    sender: 'YERROpsbjdfvcvwebfy',
+    recipient: 'ieufyygivninbsxqoPjvg'
+  }
 ];
 
-const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockDetails)
-// console.log(nonce);
-
-
+const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockDetails);
+console.log(nonce);
